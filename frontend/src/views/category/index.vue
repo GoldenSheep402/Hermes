7c24bc6145d9e26e96d7102d325ed02d8041689e
GoldenSheep <zhihanghong04@gmail.com>
2024-09-25 18:09:33 +0800

    feat(frontend): login

--- frontend/src/views/category/index.vue
+++ frontend/src/views/category/index.vue
@@ -3,7 +3,7 @@ import {CategoryService} from "@/services/grpc.ts";
 import {onMounted, ref} from "vue";
 
 interface Category {
-  id: number;
+  id: string;
   name: string;
   description: string;
 }
@@ -12,7 +12,13 @@ const categoryList = ref<Category[]>([]);
 
 function fetchCategoryList() {
   CategoryService.GetCategoryList({}).then((res) => {
-    console.log(res);
+    for (let i = 0; i < res.category!!.length; i++) {
+      categoryList.value.push({
+        id: res.category!![i].id!!,
+        name: res.category!![i].name!!,
+        description: res.category!![i].description!!,
+      });
+    }
   });
 }
 
@@ -24,6 +30,21 @@ onMounted(() => {
 <template>
   <div class="p-5">
     <div class="p-5 bg-[--color-bg-2]">
+      <a-table :data="categoryList">
+        <template #columns>
+          <a-table-column title="名称" data-index="name" :width="200"></a-table-column>
+          <a-table-column title="描述" data-index="description" :width="300"></a-table-column>
+          <a-table-column title="操作" align="center" :width="100">
+            <template #cell="{ record }">
+              <div class="w-full flex justify-center">
+                <div class="w-fit flex flex-col md:flex-row items-center gap-2">
+                  <a-button @click="">详情</a-button>
+                </div>
+              </div>
+            </template>
+          </a-table-column>
+        </template>
+      </a-table>
     </div>
   </div>
 </template>

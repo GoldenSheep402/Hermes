1b43e337ca2e2aca65532a18228965ccb410866f
GoldenSheep <zhihanghong04@gmail.com>
2024-09-26 12:55:25 +0800

    feat(frontend): torrent download v1

---
+++ frontend/src/views/torrent/index.vue
@@ -0,0 +1,123 @@
+<script lang="ts" setup>
+import {onMounted, ref} from "vue";
+import {TorrentService} from "@/services/grpc.ts";
+import {Notification} from "@arco-design/web-vue";
+
+interface TorrentMessage {
+  id: string,
+  name: string;
+  description: string;
+  categoryId: string;
+  categoryName: string;
+}
+
+const torrentList = ref<TorrentMessage[]>([]);
+
+function loadList() {
+  TorrentService.GetTorrentV1List({}).then((res) => {
+    for (let i = 0; i < res.torrents!!.length; i++) {
+      torrentList.value.push({
+        id: res.torrents!![i].id!!,
+        name: res.torrents!![i].name!!,
+        description: res.torrents!![i].description!!,
+        categoryId: res.torrents!![i].categoryId!!,
+        categoryName: res.torrents!![i].categoryName!!,
+      });
+    }
+  }).catch((err) => {
+    console.error(err);
+  });
+}
+
+function base64ToUint8Array(base64: string): Uint8Array {
+  const binaryString = window.atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+}
+
+function downloadTorrent(id: string, name: string) {
+  TorrentService.DownloadTorrentV1({id: id}).then((res) => {
+    if (res.data) {
+      const uint8Array = base64ToUint8Array(res.data);
+      const blob = new Blob([uint8Array], {type: "application/octet-stream"});
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = `${name}.torrent`;
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    }
+    handleNotification("success", "下载成功", "种子下载成功");
+  }).catch((err) => {
+    handleNotification("error", "下载失败", "种子下载失败");
+    console.error('Error downloading torrent:', err);
+  });
+}
+
+
+const handleNotification = (type: string, title: string, content: string) => {
+  switch (type) {
+    case "success":
+      Notification.success({
+        title: title,
+        content: content,
+      });
+      break;
+    case "error":
+      Notification.error({
+        title: title,
+        content: content,
+      });
+      break;
+    case "warning":
+      Notification.warning({
+        title: title,
+        content: content,
+      });
+      break;
+    default:
+      Notification.info({
+        title: title,
+        content: content,
+      });
+  }
+}
+
+onMounted(() => {
+  loadList();
+});
+</script>
+
+<template>
+  <div class="p-5">
+    <div class="p-5 bg-[--color-bg-2]">
+      <div class="p-0.5 text-20px leading-[1.4] font-500 text-[--color-text-1] mb-5">
+        种子列表
+      </div>
+
+      <a-table :data="torrentList">
+        <template #columns>
+          <a-table-column key="name" dataIndex="name" title="名称"></a-table-column>
+          <!--          <a-table-column title="描述" dataIndex="description" key="description"></a-table-column>-->
+          <a-table-column key="categoryName" dataIndex="categoryName" title="类别名称"></a-table-column>
+          <a-table-column key="action" title="操作">
+            <template #cell="{record}">
+              <a-button type="primary" @click="downloadTorrent(record.id,record.name)">下载</a-button>
+            </template>
+          </a-table-column>
+        </template>
+      </a-table>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>

35522cb8804e36c212c5cb2b1bbd410d5f82a1a4
GoldenSheep <zhihanghong04@gmail.com>
2024-10-03 00:34:14 +0800

    feat(setting): proto

---
+++ frontend/src/lib/proto/system/v1/system.pb.ts
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../fetch.pb"
+export type Settings = {
+  peerExpireTime?: number
+  allowedNets?: string[]
+  smtpEnable?: boolean
+  smtpHost?: string
+  smtpPort?: number
+  smtpUser?: string
+  smtpPassword?: string
+  registerEnable?: boolean
+  loginEnable?: boolean
+  publishEnable?: boolean
+}
+
+export type GetSettingsRequest = {
+}
+
+export type GetSettingsResponse = {
+  settings?: Settings
+}
+
+export type SetSettingsRequest = {
+  settings?: Settings
+}
+
+export type SetSettingsResponse = {
+}
+
+export class SystemService {
+  static GetSettings(req: GetSettingsRequest, initReq?: fm.InitReq): Promise<GetSettingsResponse> {
+    return fm.fetchReq<GetSettingsRequest, GetSettingsResponse>(`/gapi/system/v1/settings/get`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static SetSettings(req: SetSettingsRequest, initReq?: fm.InitReq): Promise<SetSettingsResponse> {
+    return fm.fetchReq<SetSettingsRequest, SetSettingsResponse>(`/gapi/system/v1/settings/set`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+}
